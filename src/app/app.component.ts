2ef00c32f79bbafca2d8bc9bda7263d84c21cea3
BakalDev <adambakaldev@gmail.com>
2017-04-13 08:26:20 +0100

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+import { PostcodeService } from './services/postcode.service';
+import { Postcode } from './models/postcode';
+import { PostcodeValid } from './models/postcode-valid';
+import { GoogleMapsAddress } from './models/google-maps';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [PostcodeService]
+})
+export class AppComponent {
+  postcode: Postcode;
+  search: string;
+  errorMessage: string;
+  fillAddress: boolean;
+  address: any;
+
+  constructor(private _postcodeService: PostcodeService) { }
+
+  findAddress() {
+    this._postcodeService.findAddress(this.search)
+      .subscribe(
+      res => {
+        this.address = res;
+        if (this.address) { this.fillAddress = true; }
+        console.log(this.address);
+      }
+      );
+
+if (this.fillAddress) {
+  console.log(this.address.results[0].formatted_address);
+  console.log('here');
+}
+
+  }
+
+  lookupPostcode() {
+    // this._postcodeService.lookupByPostcode(this.search)
+    //   // (data: Postcode) => this.postcode = data,
+    //   .subscribe(
+    //   res => {
+    //     this.postcode = res;
+    //     if (this.postcode) {
+    //       this.fillAddress = true;
+    //     }
+    //   },
+    //   error => this.errorMessage = <any>error);
+  }
+
+}
---
+++ src/app/services/postcode.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'; // resolves the property 'map' does not exist error
+import 'rxjs/add/operator/catch'; // resolves the property 'catch' does not exist
+
+import { Postcode } from '../models/postcode'; // maps postcode object
+import { PostcodeValid } from '../models/postcode-valid';
+import { GoogleMapsAddress } from '../models/google-maps';
+
+@Injectable()
+export class PostcodeService {
+
+  private _postcodeApiUrl: string = 'https://api.postcodes.io/postcodes/';
+  private _googleMapsApiUrl: string = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+  private _googleMapsKey: string = 'AIzaSyCQU3BvYvB6Pljst4Ry_rni89spYB1rh44';
+  constructor(private _http: Http) { }
+
+  // Return postcode object from postcode string
+  findAddress(param: string): Observable<any> {
+    return this._http.get(this._googleMapsApiUrl + param + ',+UK&key=' + this._googleMapsKey )
+      .map((response: Response) => response.json())
+      .catch(this.handleError);
+  }
+
+  isPostcodeValid(param: string): Observable<PostcodeValid> {
+    return this._http.get(this._postcodeApiUrl + param + '/validate')
+   .map((response: Response) => <PostcodeValid>response.json())
+    .catch(this.handleError);
+  }
+
+  // Handle errors.
+  private handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+}
